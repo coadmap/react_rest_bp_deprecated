c5c2eb52c7f9848ca0c06e546fd26c12085b8095
nozomi-iida <iida19990106@gmail.com>
2021-09-03 13:30:31 +0900

    chore: setup antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import 'antd/dist/antd.css';
 import "./App.scss";
 import { BrowserRouter, Route } from "react-router-dom";
 import RecruitmentIndexPage from "./scenes/recruitment/RecruitmentIndexPage";
